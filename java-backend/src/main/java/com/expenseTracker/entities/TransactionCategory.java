6e36026b00c242d8f71d606e95d02410ca6c8226
Lakshya M Sharma <lakshyasharma084@gmail.com>
2026-09-26 20:53:50 +0530

    cd error fixed 4

--- java-backend/src/main/java/com/expenseTracker/entities/TransactionCategory.java
+++ java-backend/src/main/java/com/expenseTracker/entities/TransactionCategory.java
@@ -24,7 +24,7 @@ public class TransactionCategory {
     private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = true)
     private User user;
 
     @Column(name = "category_name", nullable = false, length = 100)
